2901f271de38de5f6d71f73f87e1dcfd6f7decbb
Yulia <iqeen94@gmail.com>
2023-04-10 18:13:05 +0300

    Added ENV file

--- src/api/index.js
+++ src/api/index.js
@@ -1,59 +1,63 @@
 import axios from "axios";
 
-//const REACT_BASE_URL = "https://sephora.p.rapidapi.com/";
-const REACT_BASE_URL = "/";
+let REACT_BASE_URL;
+if (process.env.REACT_APP_ENV === "production") {
+  REACT_BASE_URL = "https://sephora.p.rapidapi.com/";
+} else {
+  REACT_BASE_URL = "/";
+}
 
 const makeUpEndpoints = {
   products: function () {
-    return process.env.ENV === "production"
+    return process.env.REACT_APP_ENV === "production"
       ? "products/v2/list"
       : "products.json";
   },
   product: function () {
-    return process.env.ENV === "production"
+    return process.env.REACT_APP_ENV === "production"
       ? "products/v2/detail"
       : "product.json";
   },
   filters: function () {
-    return process.env.ENV === "production"
+    return process.env.REACT_APP_ENV === "production"
       ? "products/v2/get-filters"
       : "filters.json";
   },
   categories: function () {
-    return process.env.ENV === "production"
+    return process.env.REACT_APP_ENV === "production"
       ? "categories/v2/list"
       : "categories.json";
   },
 };
 
-const SephoraApi = axios.create({
+const sephoraApi = axios.create({
   baseURL: REACT_BASE_URL,
   headers: {
-    "X-RapidAPI-Key": process.env.SEPHORA_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_SEPHORA_KEY,
     "X-RapidAPI-Host": "sephora.p.rapidapi.com",
   },
 });
 
 export const getProducts = (params) => {
-  return SephoraApi.get(makeUpEndpoints.products(), {
+  return sephoraApi.get(makeUpEndpoints.products(), {
     params: params,
   });
 };
 
 export const getProduct = (params) => {
-  return SephoraApi.get(makeUpEndpoints.product(), {
+  return sephoraApi.get(makeUpEndpoints.product(), {
     params: params,
   });
 };
 
 export const getFilters = (params) => {
-  return SephoraApi.get(makeUpEndpoints.filters(), {
+  return sephoraApi.get(makeUpEndpoints.filters(), {
     params: params,
   });
 };
 
 export const getCategories = (params) => {
-  return SephoraApi.get(makeUpEndpoints.categories(), {
+  return sephoraApi.get(makeUpEndpoints.categories(), {
     params: params,
   });
 };
@@ -61,10 +65,11 @@ export const getCategories = (params) => {
 const monobankApi = axios.create({
   baseURL: "https://api.monobank.ua/",
   headers: {
-    "X-Token": "uZh2yQb49lwPZapVEUqkY2RJdJ-YSN_tsYiAAdkP9Gxk",
+    "X-Token": process.env.REACT_APP_MONOBANK_KEY,
   },
 });
 
 export const getInvoice = (params) => {
   return monobankApi.post("api/merchant/invoice/create", params);
 };
+console.log(process.env);
